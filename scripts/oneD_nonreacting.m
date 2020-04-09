8b8e259e08a03f5f182dbd958a14b55e54d6b454
mukher25 <mukher25@purdue.edu>
2020-04-09 17:48:24 -0400

    dx

--- scripts/oneD_nonreacting.m
+++ scripts/oneD_nonreacting.m
@@ -9,7 +9,7 @@ clc;
 %% Properties - Table 6-1
 p_inf               = 6.6e4;            % Pa
 T_inf               = 1200;             % K
-M_inf               = 6;                % Mach number
+M_inf               = 0.1;                % Mach number
 gamma               = 1.4;
 R                   = 8.314;
 u_inf               = M_inf*sqrt(gamma*R*T_inf);
@@ -20,7 +20,7 @@ cv_O2               = cp_O2*gamma;
 cv_O               = cp_O*gamma;
 L                   = 0.213;            % m
 n_grid              = 129;              
-CFL                 = 0.9;
+CFL                 = 0.1;
  
 %% Pre processing
 % Grid
@@ -68,12 +68,12 @@ while (diff > 1e-8)
     
     % Upwinding convection terms
     % Mass conservation - central nodes
-    dFdx(1,2:end-1) = ((rho(2:end-1).*u(2:end-1)).*((A(2:end-1)+A(3:end))/2)) - ...
-                   ((rho(1:end-2).*u(1:end-2)).*((A(2:end-1)+A(1:end-2))/2)); 
+    dFdx(1,2:end-1) = (((rho(2:end-1).*u(2:end-1)).*((A(2:end-1)+A(3:end))/2)) - ...
+                   ((rho(1:end-2).*u(1:end-2)).*((A(2:end-1)+A(1:end-2))/2)))/dx; 
     % Mass conservation - right boundary
-    dFdx(1,end) = rho(end)*u(end)*interp1(x,A,L) - rho(end-1)*u(end-1)*((A(end)+A(end-1))/2);
+    dFdx(1,end) = (rho(end)*u(end)*interp1(x,A,L) - rho(end-1)*u(end-1)*((A(end)+A(end-1))/2))/(dx/2);
     % Mass conservation - left boundary
-    dFdx(1,1) = rho(1)*u(1)*((A(1)+A(2))/2) - rho_inf*u_inf*interp1(x,A,0);
+    dFdx(1,1) = (rho(1)*u(1)*((A(1)+A(2))/2) - rho_inf*u_inf*interp1(x,A,0))/(dx/2);
     
     % Momentum conservation - central nodes
     dFdx(2,2:end-1) = (((rho(2:end-1).*u(2:end-1).*u(2:end-1)).*((A(2:end-1)+A(3:end))/2)) - ...
@@ -82,21 +82,21 @@ while (diff > 1e-8)
     
     % Momentum conservation - right boundary
     dFdx(2,end) = (rho(end)*u(end)*u(end)*interp1(x,A,L) - rho(end-1)*u(end-1)*u(end-1)*((A(end)+A(end-1))/2) + ...
-               p_inf*interp1(x,A,L) - ((p(end-1)+p(end))/2)*((A(end)+A(end-1))/2))/dx;
+               p_inf*interp1(x,A,L) - ((p(end-1)+p(end))/2)*((A(end)+A(end-1))/2))/(dx/2);
            
     % Momentum conservation - left boundary
     dFdx(2,1) = (rho(1)*u(1)*u(1)*((A(1)+A(2))/2) - rho_inf*u_inf*u_inf*interp1(x,A,0) + ...
-             ((p(2)+p(1))/2)*((A(2)+A(1))/2) - p_inf*interp1(x,A,0))/dx;
+             ((p(2)+p(1))/2)*((A(2)+A(1))/2) - p_inf*interp1(x,A,0))/(dx/2);
          
     % Energy conservation - central nodes
     dFdx(3,2:end-1) = (cp*rho(2:end-1).*T(2:end-1).*u(2:end-1).*((A(2:end-1)+A(3:end))/2) - ...
                     cp*rho(1:end-2).*T(1:end-2).*u(1:end-2).*((A(2:end-1)+A(1:end-2))/2))/dx;
     
     % Energy conservation - right boundary
-    dFdx(3,end) = cp*rho(end)*u(end)*T(end)*interp1(x,A,L) - cp*rho(end-1)*u(end-1)*T(end-1)*((A(end)+A(end-1))/2);
+    dFdx(3,end) = (cp*rho(end)*u(end)*T(end)*interp1(x,A,L) - cp*rho(end-1)*u(end-1)*T(end-1)*((A(end)+A(end-1))/2))/(dx/2);
     
     % Energy conservation - left boundary
-    dFdx(3,1) = cp*rho(1)*u(1)*T(1)*((A(1)+A(2))/2) - cp*rho_inf*u_inf*T_inf*interp1(x,A,0);
+    dFdx(3,1) = (cp*rho(1)*u(1)*T(1)*((A(1)+A(2))/2) - cp*rho_inf*u_inf*T_inf*interp1(x,A,0))/(dx/2);
     
     % H vector
     H = zeros(3,n_grid);
@@ -122,7 +122,7 @@ while (diff > 1e-8)
     T = (Unew(3,:)./(A.*rho)) - 0.5.*u.*u;
     
     plot(p);
-    pause(0.1);
+%     pause(0.1);
 end
 
 
