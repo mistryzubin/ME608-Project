6f4b1338d31d9eebe26c0ff27cde2f5baf4ef5c1
mukher25 <mukher25@purdue.edu>
2020-04-12 20:23:33 -0400

    Reacting flow 1D

--- scripts/oneD_nonreacting.m
+++ scripts/oneD_nonreacting.m
@@ -1,6 +1,5 @@
 % Bussing thesis - Chapter 6 - Non-Reacting flow plots
 % Essentially euler equations (2.39) with no reaction source
-% MacCormack point implicit method - Implicit only on the reaction source 
 
 close all;
 clear;
@@ -48,13 +47,11 @@ u = u_inf*ones(1,n_grid);
 T = T_inf*ones(1,n_grid);
 p = p_inf*ones(1,n_grid);
 rho = rho_inf*ones(1,n_grid);
-YO2 = ones(1,n_grid);
-YO = zeros(1,n_grid);
 
 % Lets do this explicitly till steady state
 diff = 1;
 t = 0;
-while (diff > 1e-8)
+while (diff > 1e-5)
     % Guess a value of dt based on intial velocity
     dt = min((CFL*dx/max(u)),100);
     t = t + dt;
@@ -81,34 +78,31 @@ while (diff > 1e-8)
     dFdx(3,1) = dFdx(3,1) + (p(1)*u(1)*A(0.5*(x(1)+x(2))) - p_inf*u_inf*A(0))/dx;
     dFdx(3,end) = dFdx(3,end) + (p(end)*u(end)*A(L) - p(end-1)*u(end-1)*A(0.5*(x(end)+x(end-1))))/dx;
     
-%     dFdx(4,2:end-1) = (rho(2:end-1).*u(2:end-1).*A(0.5*(x(2:end-1)+x(3:end))).*YO2(2:end-1) - ...
-%                       rho(1:end-2).*u(1:end-2).*A(0.5*(x(2:end-1)+x(1:end-2))).*YO2(1:end-2))/dx;
-%     dFdx(4,1) = (rho(1)*u(1)*A(0.5*(x(2)+x(1)))*YO2(1) - tho_inf*u_inf*A(0)*1)/dx;
-%     dFdx(4,end) = (rho(end)*u(end)*A(L)*YO2(end) - rho(end-1)*u(end-1)*A(0.5*(x(end)+x(end-1)))*YO2(end-1))/dx;
-%     
-%     dFdx(5,2:end-1) = (rho(2:end-1).*u(2:end-1).*A(0.5*(x(2:end-1)+x(3:end))).*YO(2:end-1) - ...
-%                       rho(1:end-2).*u(1:end-2).*A(0.5*(x(2:end-1)+x(1:end-2))).*YO(1:end-2))/dx;
-%     dFdx(5,1) = (rho(1)*u(1)*A(0.5*(x(2)+x(1)))*YO(1) - tho_inf*u_inf*A(0)*0)/dx;
-%     dFdx(5,end) = (rho(end)*u(end)*A(L)*YO(end) - rho(end-1)*u(end-1)*A(0.5*(x(end)+x(end-1)))*YO(end-1))/dx;
-
+    % Put the momentum source in dFdx
+    dFdx(2,2:end-1) = dFdx(2,2:end-1)-p(2:end-1).*(A(0.5*(x(2:end-1)+x(3:end)))-A(0.5*(x(2:end-1)+x(1:end-2))))/dx;
+    dFdx(2,1) = dFdx(2,1)-p(1)*(A(0.5*(x(2)+x(1)))-A(0))/dx;
+    dFdx(2,end) = dFdx(2,end)-p(end)*(A(L)-A(0.5*(x(end)+x(end-1))))/dx;
     
     H = zeros(3,n_grid);
-    H(2,2:end-1) = -p(2:end-1).*((A(0.5*(x(2:end-1)+x(3:end)))-A(0.5*(x(2:end-1)+x(1:end-2))))/dx);
-    H(2,1) = -p(1)*((A(x(1))-A(0))/(dx/2));
-    H(2,end) = -p(end)*((A(L)-A(x(end)))/(dx/2));
+%     H(2,2:end-1) = -p(2:end-1).*((A(0.5*(x(2:end-1)+x(3:end)))-A(0.5*(x(2:end-1)+x(1:end-2))))/dx);
+%     H(2,1) = -p(1)*((A(x(1))-A(0))/(dx/2));
+%     H(2,end) = -p(end)*((A(L)-A(x(end)))/(dx/2));
     
-    
-    
+
     Uold = zeros(3,n_grid);
     Uold(1,:) = rho.*A(x);
     Uold(2,:) = rho.*u.*A(x);
     Uold(3,:) = rho.*A(x).*(cv_O2*T + 0.5*u.*u);
+    
+    % Pure explicit
     
     Unew = Uold - dt*(dFdx + H);
     rho = Unew(1,:)./A(x);
     u = Unew(2,:)./(rho.*A(x));
     T = (Unew(3,:)./(rho.*A(x)) - 0.5*u.*u)/cv_O2;
     T = T + T_inf;
+    
+    
     
     temp = ((rho).*(T).*(R/0.032));
    
@@ -119,7 +113,7 @@ while (diff > 1e-8)
     p = temp;
     M = u./sqrt(gamma*(R/0.032)*T);
     
-    plot(x/L,p/(rho_inf*u_inf*u_inf));
+    plot(x/L,M);
     pause(0.000001);
 end
 
